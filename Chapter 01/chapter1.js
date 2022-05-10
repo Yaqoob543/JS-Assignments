656353bbdb982ecc9f78c8409e7a1a47bd7ddfb7
SualehFarooq <sualehfarooq65@gmail.com>
2022-05-10 23:18:18 +0500

    commit
    Assignment of JS Chapter 1 & 2

---
+++ Chapter 01/chapter1.js
@@ -0,0 +1,20 @@
+
+// Answer 1
+function task1() {
+window.alert("Welcome To My Website");
+}
+//Answer 2
+function task2() {
+window.alert("Error! Please enter a valid password.");
+}
+// Answer 3
+function task3() {
+window.alert("Welcome to JS Land,,,, \nHappy Coding!");
+}
+
+//Answer 4
+function task4(){
+window.alert("Welcome to JS Land,,,,");
+window.alert("Happy Coding");
+}
+
---
+++ Chapter 02/chapter2.js
@@ -0,0 +1,52 @@
+//Task 1 
+var username;
+
+
+//Task 2
+var myName = "Yaqoob Ahmed";
+
+//Task 3
+function task3() {
+    var message = "Hello World";
+    alert(message);
+}
+
+//Task 4 
+function task4 () {
+    var name = "John Doe";
+    var age = "15 Years Old";
+    var certificate = "Certified Mobile Application Development";
+
+    alert(name);
+    alert(age);
+    alert(certificate);
+}
+
+//Task 5 
+function task5 () {
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(pizza);
+}
+
+//Task 6 
+function task6 () {
+    var email = "yaqoobfarooq@gmail.com";
+    alert ("My email address is : " + email);
+}
+
+//Task 7
+function task7 () {
+    var book = "A Smarter Way to Learn Javascript";
+    alert(book);
+}
+
+//Task 8 
+function task8 () {
+    document.write ("<h1>", "Yah! I can Write HTML Content through Javascript","</h1>");
+}
+
+//Task 8 
+function task9() {
+    var task9 = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+    alert(task9);
+}
