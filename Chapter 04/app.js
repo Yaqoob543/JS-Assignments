6e35279082e410994368b560567b8d5dad921bb6
SualehFarooq <sualehfarooq65@gmail.com>
2022-05-16 22:40:14 +0500

    commit

---
+++ Chapter 04/app.js
@@ -0,0 +1,37 @@
+
+//Task 1
+function task1() {
+    var name, age, qualification;
+}
+
+//Task 2
+function task2() {
+    //5 Legal Variable Names
+    var name;
+    var number2;
+    var F16Thunder;
+    var $_;
+    var sign$;
+
+    //5 illegal Variable Names
+    // var 2 ;
+    // var product name ;
+    // var ^ ;
+    // var var ;
+    // var 2times ;
+}
+
+
+//Task 3 
+function task3() {
+    var heading = "Rules for naming JS Variables"
+    document.write("<h1>", heading, "</h1>");
+    ("<br>") +
+        ("<br>")
+
+
+    document.write("<p>", "Variable names can only contain , numbers, $ and _. For Example :$my_1stVariable", "</p>");
+    document.write("<p>", "Variable must begin with a letter, $ or _. for example : $name, _name or name", "</p>")
+    document.write("<p>", "Variable names are case Sensitive", "</p>");
+    document.write("<p>", "Variable names can't be JS Keywords", "</p>");
+}
