5dd845b829a6d01aad777ad14ec343dbb24f3d0a
SualehFarooq <sualehfarooq65@gmail.com>
2022-05-16 21:55:59 +0500

    commit
    Chapter 03 Assignment

---
+++ Chapter 03/chapter3.js
@@ -0,0 +1,28 @@
+
+//Task 1
+function task1() {
+    var age;
+    age = 20;
+    alert("I am " + age + " years old .");
+}
+
+//Task 2 
+function task2() {
+    var visit = 3;
+    alert("You have visited this site " + visit + " times")
+}
+
+//Task 3 
+function task3() {
+        var birth_Year = 2001;
+        document.write("<h1>", "My Birth Year is " + birth_Year , "</h1>" );
+        document.write("<h1>", "Data type of my varible is " + typeof(birth_Year) , "</h1>");
+}
+
+//Task 4 
+function task4() {
+    var visitor_name = "John Doe";
+    var product = "T-shirts(s)"
+    var quantity = 5;
+    document.write("<h2>" , visitor_name + " ordered " + quantity + " " + product + " On XYZ Clothing Store" , "</h2>")
+}
